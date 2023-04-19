df2cbf307eae4563d3a9ed337d5531409cc7e4bb
AOrps <aorbeandrews@gmail.com>
2023-04-19 11:44:42 -0400

    local file sync if not exists

--- lib/cachelike.py
+++ lib/cachelike.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from typing import Callable
-from enum import Enum
 import os
 import os.path
 import requests
@@ -10,15 +9,12 @@ cache_map = {
     "linknyc_locations": "s4kf-3yrf",
     "linknyc_kiosk_status": "n6c5-95xh",
     "nyc_wifi_hotspot_location": "yjub-udmw"
-    }
+}
 
-data_dir = ""
-
-# End = Enum('End',['CSV','JSON','GEOJSON'])
     
 api_url_retrieval: Callable[[str,str],str] = lambda document,end="csv" : f"https://data.cityofnewyork.us/resource/{document}.{end}"
 api_url_retrieval.__doc__ = """
-api_url_retrieval: get's the url base from a particular thing
+api_url_retrieval: get's the url base from a particular dataset
 
 Potential File Endings:
 - csv
@@ -26,32 +22,22 @@ Potential File Endings:
 - geojson
 """
 
-check_file: Callable[[str], bool] = lambda path: os.path.isfile(path)
-
 def sync_up(file_type='csv'):
-    """
+    """Brings new files if they don't exist locally
     
+    Keyword arguments:
+    file_type -- the ending file type, file_type available: 'csv','json','geojson'
     """
-    print(file_type)
     for k in cache_map:
-        retrieval_url = f"{api_url_retrieval(k,file_type)}"
-        # file does not exist locally
-        if(not check_file(retrieval_url)):
-            
-            print(f"make new {retrieval_url}")
-
-            
+        ret_f = f"{k}.{file_type}"
+        retrieval_url = f"{api_url_retrieval(cache_map[k],file_type)}"
+        if(not os.path.isfile(ret_f)):
+            req = requests.get(retrieval_url)
+            with open(f"{ret_f}", "w") as f:
+                f.write(req.text)
+            print(f"\tCreated new file: {ret_f}")
 
 
 if __name__ == "__main__":
-    for i,k in enumerate(cache_map):
-        v = cache_map[k]
-        print(f"{k}\t{v}\t{check_file(k)}")
-        print(f"{api_url_retrieval(k)}")
-        print(f"{api_url_retrieval(k,'json')}")        
-
-
     sync_up()
-    sync_up('json')
-    # print(list(End))
     pass
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 # load_dotenv(): loads .env file into the running process
 load_dotenv()
 
+# sync_up(): loads data files if they don't exist
+sync_up()
 
 
 if __name__ == "__main__":
